f4b11e4a57f68c26e18cbdbf1d2a2487844beb9e
C SUMANTH <121371221+SUMANTHCHINNA@users.noreply.github.com>
2026-02-28 15:39:32 +0000

    Logout API Done

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,7 @@ const createUserController = async (req, res) => {
         let hasshedPassword = await bycrypt.hash(user_pass, 10);
         const result = await createUser(userId, full_name, user_email, hasshedPassword);
         const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
         res.status(201).json({ message: 'User registered successfully', user: result.rows[0], token });
     } catch (err) {
         console.error('Error registering user:', err);
@@ -21,6 +22,7 @@ const createUserController = async (req, res) => {
 
 const userLoginController = async (req, res) => {
     const { user_email, user_pass } = req.body;
+    console.log(req.body);
     if (!user_email || !user_pass) {
         return res.status(400).json({ message: 'Email and password are required' });
     }
@@ -35,6 +37,7 @@ const userLoginController = async (req, res) => {
             return res.status(401).json({ message: 'Invalid email or password' });
         }
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
         res.status(200).json({ message: 'Login successful', user: { id: user.id, full_name: user.full_name, user_email: user.user_email }, token });
     } catch (err) {
         console.error('Error during login:', err);
@@ -42,7 +45,14 @@ const userLoginController = async (req, res) => {
     }
 }
 
+
+const logoutController = (req, res) => {
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logout successful' });
+}
+ 
 module.exports = {
     createUserController,
-    userLoginController
+    userLoginController,
+    logoutController
 };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createUserController,userLoginController } = require('../controllers/authController');
+const { createUserController,userLoginController,logoutController } = require('../controllers/authController');
 const dotenv = require('dotenv');
 dotenv.config();
 
 router.post('/createUser',createUserController);   
 router.post('/login', userLoginController); 
+router.post('/logout' , logoutController);
 
 module.exports = router;
