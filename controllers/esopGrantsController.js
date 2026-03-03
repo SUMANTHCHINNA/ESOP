98fd2edfdefd7eaeedd27b1608617c0449965735
C SUMANTH <121371221+SUMANTHCHINNA@users.noreply.github.com>
2026-03-03 11:28:59 +0000

    Working on Create Grants

--- controllers/esopGrantsController.js
+++ controllers/esopGrantsController.js
@@ -1,11 +1,26 @@
-const {createGrantsService,getGrantDetailsService} = require('../services/esopGrantsService')
+const { createGrantsService, getGrantDetailsService } = require('../services/esopGrantsService')
 
-const createGrantsController = async(req,res) =>{
-    await createGrantsService(req.body);
+const createGrantsController = async (req, res) => {
+    try {
+        const adminId = req.user.id;
+        const grantDetails = await createGrantsService(req.body, adminId);
 
+        return res.status(201).json({
+            message: 'ESOP Grants created successfully',
+            grantDetails: grantDetails
+        });
+
+    } catch (err) {
+        console.error('Error In createGrantsController:', err.message);
+
+        const statusCode = err.statusCode || 500;
+        return res.status(statusCode).json({
+            message: err.message || 'Internal server error'
+        });
+    }
 }
 
-const getGrantDetailsController = async(req ,res) =>{
+const getGrantDetailsController = async (req, res) => {
     await getGrantDetailsService(req.body);
 }
 
